fcec508a8072f05d33c8bfb3a8546418711c034b
Davandres <160764226+Davandres@users.noreply.github.com>
2026-05-26 05:38:07 -0500

    version final 1.0

--- include/mime.h
+++ include/mime.h
@@ -1,7 +1,9 @@
 #ifndef MIME_H
 #define MIME_H
 
-// Obtener el tipo MIME según la extensión del archivo
-const char* get_mime_type(const char *filename);
+// Dado un nombre de archivo como "index.html",
+// devuelve el tipo MIME correspondiente, ej: "text/html"
+// Si la extensión no se reconoce, devuelve "application/octet-stream"
+const char *mime_get_type(const char *filename);
 
 #endif
