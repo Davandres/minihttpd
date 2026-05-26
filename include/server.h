6da5831ca17086f28e6475df1dbc1da09e41785e
Davandres <160764226+Davandres@users.noreply.github.com>
2026-05-26 05:39:42 -0500

    version final 1.0

--- include/server.h
+++ include/server.h
@@ -1,33 +1,9 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include <sys/epoll.h>
-
-#define MAX_EVENTS 100
-#define PORT 8080
-#define BUFFER_SIZE 16384
-#define MAX_CLIENTS 1000
-
-// Estructura para manejar conexiones de clientes
-typedef struct {
-    int fd;
-    char buffer[BUFFER_SIZE];
-    int buffer_length;
-    int keep_alive;
-} client_info;
-
-// Estructura del servidor
-typedef struct {
-    int server_fd;
-    int epoll_fd;
-    struct epoll_event events[MAX_EVENTS];
-    client_info clients[MAX_CLIENTS];
-    char root_dir[256];
-} server;
-
-// Funciones del servidor
-int init_server(server *srv, int port, const char *root_dir);
-void run_server(server *srv);
-void cleanup_server(server *srv);
+/* Inicializa el servidor: crea el socket con getaddrinfo,
+ * hace bind y listen.
+ * Devuelve el file descriptor del socket servidor, o -1 en error. */
+int server_init(int port);
 
 #endif
